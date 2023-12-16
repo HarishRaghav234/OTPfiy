741f519d29a4f502d28289bc2a350f0484092b03
gururock007 <gurushiktemp@gmail.com>
2023-12-17 00:05:10 +0530

    initalizing Frontend Files

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className=' text-center font-serif text-9xl text-white '>OTPfiy</div>
+    </>
+  )
+}
+
+export default App
