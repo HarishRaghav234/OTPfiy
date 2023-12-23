173c13aaf928f93c71e23a10e29f6160be38805f
gururock007 <gurushiktemp@gmail.com>
2023-12-24 00:29:25 +0530

    ADDED AXIOS tested with proxy server for api fecthing

--- src/components/LoginForms.jsx
+++ src/components/LoginForms.jsx
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion"
 import { useState } from "react"
 import { Link } from "react-router-dom"
 
@@ -9,6 +10,10 @@ const LoginForms = () => {
     const [vaildData,setVaildData] = useState({
         Email:false,
         Password:false,
+    })
+    const [errorMsg,setErrorMsg] = useState({
+        title:"",
+        message:"",
     })
 
     function isValidGmail(email) {
@@ -39,13 +44,35 @@ const LoginForms = () => {
             }))
         }
     }
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        const mail =  isValidGmail(registationData.Email) 
+        const pwd =  isValidPassword(registationData.Password)
+        if(!mail | !pwd ){
+            setErrorMsg({title:"wrong",message:"Wrong",value:true})
+            return
+        }else{setErrorMsg({title:"true",message:"true",value:true})}
+    }
   return (
     <section>
+         {errorMsg.value && 
+        <motion.div 
+        className="
+        w-full md:w-1/2 mx-auto p-2
+        flex flex-row gap-3 justify-center 
+        text-white text-lg  
+        rounded-lg bg-red-200/50 "
+        initial= {{y:-10,opacity:0}}
+        animate ={{y:0,opacity:1, transition:{duration:1}}}
+        >
+            <div className=" text-center font-medium">{errorMsg.title}</div>
+            <div className=" text-center font-light italic">{errorMsg.message}</div>
+        </motion.div>}
         <div className=' container mx-auto bg-[url(/Images/Turtle-bg.png)] bg-no-repeat bg-right-bottom text-white mt-10 h-[27rem]'>
             <div className=" flex justify-center items-center">
                 <div 
                 className=" 
-                p-5 md:w-1/3 
+                p-5 lg:w-1/3 md:w-1/2 w-3/4
                 rounded-3xl border 
                 bg-gradient-to-b from-white/[0.15] to-white/[0.08] 
                 border-gray-100 bg-clip-padding 
@@ -55,11 +82,11 @@ const LoginForms = () => {
                         <div className=" p-5 text-center text-2xl">
                             Log In
                         </div>
-                        <form>
+                        <form onSubmit={handleSubmit}>
                             <div className={
                                 vaildData.Email ?
-                                " bg-transparent border py-1 mt-2 rounded-lg focus-within:border-green-500":
-                                " bg-transparent border py-1 mt-2 rounded-lg focus-within:border-red-500"
+                                " bg-transparent border py-1 mt-2 rounded-lg focus-within:border-green-500 focus-within:shadow-lg focus-within:shadow-green-500":
+                                " bg-transparent border py-1 mt-2 rounded-lg focus-within:border-red-500 focus-within:shadow-lg focus-within:shadow-red-500"
                                 }>
                                 <input 
                                 className="bg-transparent h-10 w-full p-2 font-extralight focus:outline-none " 
@@ -71,8 +98,8 @@ const LoginForms = () => {
                             </div>
                             <div className={
                                 vaildData.Password ?
-                                " bg-transparent border py-1 mt-2 rounded-lg focus-within:border-green-500":
-                                " bg-transparent border py-1 mt-2 rounded-lg focus-within:border-red    -500"
+                                " bg-transparent border py-1 mt-2 rounded-lg focus-within:border-green-500 focus-within:shadow-lg focus-within:shadow-green-500":
+                                " bg-transparent border py-1 mt-2 rounded-lg focus-within:border-red-500 focus-within:shadow-lg focus-within:shadow-red-500"
                             }>
                                 <input 
                                 className="bg-transparent h-10 w-full p-2 font-extralight focus:outline-none " 
@@ -86,6 +113,7 @@ const LoginForms = () => {
                                 <div className=" grid grid-cols-1 gap-10">
                                     <button 
                                     disabled = {!vaildData.Email | !vaildData.Password}
+                                    type="submit"
                                     className="text-center
                                     border rounded-3xl
                                     hover:bg-[rgba(210,223,255,.156)] 
--- src/components/SignUpForms.jsx
+++ src/components/SignUpForms.jsx
@@ -1,5 +1,14 @@
-import {  useState } from "react"
+import { motion } from "framer-motion"
+import { useState } from "react"
 import { Link } from "react-router-dom"
+import axios from "../api/axios"
+
+const REGEX_EMAIL = /^[^\s@]+@gmail\.com$/;
+const REGEX_PASSWORD = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/;
+const REGEX_OTP = /^\d{4}$/;
+const SIGNUP_URL = '/signup'
+const GET_OTP_URL = '/otp'
+
 
 const SignUpForms = () => {
 
@@ -12,17 +21,23 @@ const SignUpForms = () => {
         Password:false,
         Otp:false
     })
+    const [errorMsg,setErrorMsg] = useState({
+        error:false,
+        value:false,
+        title:"",
+        message:"",
+    })
 
     function isValidGmail(email) {
-        return  /^[^\s@]+@gmail\.com$/.test(String(email).trim().toLowerCase());
+        return  REGEX_EMAIL.test(String(email).trim().toLowerCase());
     }
 
     function isValidPassword(password) {
-        return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/.test(String(password).trim());
+        return REGEX_PASSWORD.test(String(password).trim());
     }
 
     function isValidOtp(otp) {
-        return /^\d{4}$/.test(String(otp).trim());
+        return REGEX_OTP.test(String(otp).trim());
     }
 
     function handleFormData (event) {
@@ -52,19 +67,76 @@ const SignUpForms = () => {
         }
     }
 
-    function handleSubmit (event) {
+    const showPopUp = ({error, title, message}) => {
+        setErrorMsg({error: error, value:true, title:title, message:message})
+        setTimeout(() => {
+            setErrorMsg({value:false})
+        },3000)
+    } 
+
+    const handleSubmit = async (event) => {
         event.preventDefault();
-        console.log(registationData)
+        const email =  isValidGmail(registationData.Email) 
+        const pwd =  isValidPassword(registationData.Password)
+        if(!email | !pwd ){
+            setErrorMsg({title:"Error",message:"Check Your Email and password",value:true})
+            return
+        }
+        try {
+            const response = await axios.post(SIGNUP_URL,
+                JSON.stringify({email:registationData.Email, password: registationData.Password,apppass:'Guru@123nvh'}),
+                {
+                    headers: { "Content-Type": 'application/json'},
+                }
+            )
+            console.log(response.data)
+            console.log(response.status)
+            showPopUp({error:false, title:'success'})
+        } catch (error) {
+            if( !error?.response){ showPopUp({ error:true, title:'Error', message:'No server responce'});}
+            else{showPopUp({error:true, title:'Error',message:'Sign Up Failed'});}
+        }
     }
 
-    function handleGetOTP (event) {
+    const handleGetOTP = async (event) => {
         event.preventDefault();
-        console.log(registationData)
+        const email = isValidGmail(registationData.Email)
+        if (!email){showPopUp({error:true,title:"Error",message:"Check Your Email"});return}
+        try{
+            const response = await axios.post(GET_OTP_URL,
+                JSON.stringify(registationData.Email),
+                {
+                    headers: { "Content-Type": 'application/json'},
+                }
+            )
+            console.log(response.data)
+            console.log(response.status)
+            showPopUp({error:false, message:'otp sent'})
+        }catch (error){
+            if( !error?.response){ showPopUp({error:true, title:'Error', message:'No server responce'})}
+            else{
+                showPopUp({error:true , title:'Error',message:'Sign Up Failed'})
+            }
+        }
     }
 
   return (
     <section>
-        <div className=' container mx-auto bg-[url(/Images/Turtle-bg.png)] bg-no-repeat bg-right-bottom text-white mt-10'>
+        {
+        errorMsg.value && 
+        <motion.div 
+        className={errorMsg.error ? 
+        "w-full md:w-1/2 mx-auto p-2 flex flex-row gap-3 justify-center text-white text-lg  rounded-lg bg-red-200/50 ":
+        "w-full md:w-1/2 mx-auto p-2 flex flex-row gap-3 justify-center text-white text-lg  rounded-lg bg-green-200/50 "
+        }
+        initial= {{y:-10,opacity:0}}
+        animate ={{y:0,opacity:1, transition:{duration:1}}}
+        >
+            <div className=" text-center font-medium">{errorMsg.title}</div>
+            <div className=" text-center font-light italic">{errorMsg.message}</div>
+        </motion.div>
+        }
+        <div className=' container mx-auto bg-[url(/Images/Turtle-bg.png)] bg-no-repeat bg-right-bottom text-white mt-2'>
             <div className=" flex justify-center items-center">
                 <div className=" p-5 
                     lg:w-1/3 md:w-1/2 w-3/4
@@ -76,22 +148,27 @@ const SignUpForms = () => {
                         <div className=" p-5 text-center text-2xl">
                             Sign Up
                         </div>
-                        <form>
+                        <form onSubmit={handleSubmit}>
                             <div 
                             className={
                                 vaildData.Email ? 
-                                " bg-transparent border py-1 mt-2 rounded-lg focus-within:border-green-500 focus-within:shadow-lg focus-within:shadow-green-500":
+                                " bg-transparent border py-1 mt-2 rounded-lg focus-within:border-green-500 focus-within:shadow-lg focus-within:shadow-green-500 ":
                                 " bg-transparent border py-1 mt-2 rounded-lg focus-within:border-red-500 focus-within:shadow-lg focus-within:shadow-red-500"
                             }>
                                 <input 
-                                    className=" bg-transparent h-10 w-full p-2 font-extralight  focus:outline-none" 
+                                    className=" bg-transparent h-10 w-full p-2 font-extralight  focus:outline-none peer"  
                                     placeholder="Email" 
                                     type="text" 
                                     name="Email" 
                                     value={registationData.Email}
                                     onChange={handleFormData} 
                                     required />
+                                <div className=" absolute w-5/6 p-2 text-xs text-white/50 justify-center text-center bg-black/70 md:ms-4 ms-2 rounded-lg invisible peer-focus:visible"> 
+                                    <ul>
+                                        <li>Provide only Gmail not other email address is accepted</li>
+                                    </ul>
                                 </div>
+                            </div>
                             <div 
                             className={
                                 vaildData.Password?
@@ -99,13 +176,20 @@ const SignUpForms = () => {
                                 " bg-transparent border py-1 mt-2 rounded-lg focus-within:border-red-500 focus-within:shadow-lg focus-within:shadow-red-500"
                                 }>
                                 <input 
-                                    className="bg-transparent h-10 w-full p-2 font-extralight focus:outline-none " 
+                                    className="bg-transparent h-10 w-full p-2 font-extralight focus:outline-none peer" 
                                     placeholder="Password" 
                                     type="password" 
                                     name="Password" 
                                     value={registationData.Password}
                                     onChange={handleFormData}
                                     required />
+                                <div className=" absolute w-5/6 p-2 text-xs text-white/50 justify-center text-center bg-black/70 md:ms-4 ms-2 rounded-lg invisible peer-focus:visible"> 
+                                    <ul>
+                                        <li>atleat 8 charcters</li>
+                                        <li>Must have atlest one uppercase and lowercase</li>
+                                        <li>Must have atlest one number</li>
+                                    </ul>
+                                </div>
                             </div>
                             <div 
                             className={
@@ -114,12 +198,17 @@ const SignUpForms = () => {
                                 " bg-transparent border py-1 mt-2 rounded-lg focus-within:border-red-500 focus-within:shadow-lg focus-within:shadow-red-500"
                                 }>
                                 <input 
-                                    className="bg-transparent h-10 w-full p-2 font-extralight focus:outline-none" 
+                                    className="bg-transparent h-10 w-full p-2 font-extralight focus:outline-none peer" 
                                     placeholder="OTP" 
                                     name="OTP"
                                     type="numeric"
                                     onChange={handleFormData}
                                     required />
+                                     <div className=" absolute w-5/6 p-2 text-xs text-white/50 justify-center text-center bg-black/70 md:ms-4 ms-2 rounded-lg invisible peer-focus:visible"> 
+                                        <ul>
+                                            <li>Get the OTP by clicking out get OTP button below</li>
+                                        </ul>
+                                    </div>
                             </div>
                             <div className="py-10 text-[rgba(252,252,252,.710)]">
                                 <div className=" grid grid-cols-2 gap-10">
@@ -144,10 +233,9 @@ const SignUpForms = () => {
                                     <button
                                     type="submit"
                                     disabled = {!vaildData.Email | !vaildData.Otp | !vaildData.Password}
-                                    onClick={handleSubmit}
                                     className="
                                         text-center
-                                        border rounded-3xl
+                                        border rounded-3xl border-green-500
                                         hover:bg-[rgba(210,223,255,.156)] 
                                         hover:text-white 
                                         hover:border-[rgba(210,223,255,.156)] 
